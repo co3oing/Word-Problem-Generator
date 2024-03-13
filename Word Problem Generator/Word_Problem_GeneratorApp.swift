8d82a26a62b757e84958787016b6e713ffd6fe1b
보름_박정아 <co3oing@afreecatv.com>
2024-03-13 13:32:39 +0900

    Initial Commit

---
+++ Word Problem Generator/Word_Problem_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Problem_GeneratorApp.swift
+//  Word Problem Generator
+//
+//  Created by Boreum on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_Problem_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
